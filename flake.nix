aec808fa84fffec4cc3142ac0b64ad2e5cb8e2aa
SEUNGWOO LEE <seungwoo.lee.518@proton.me>
2023-10-22 15:07:06 +0900

    chore(flake.nix): initialize flake.nix
     * code is derived from "Super fast nix-shell" on
     https://nixos.wiki/wiki/Flakes

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "nix environment for nbd.rs implementation";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
